d8ac8819ea24b5e76b29508149d22cb0f9ac2980
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-30 16:29:59 +0200

    add data byte arrays

--- Client.java
+++ Client.java
@@ -6,6 +6,8 @@ public class Client {
   public static void main(String[] args) throws Exception {
     InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
     int port = 5298; 
+    byte[] senddata = new byte[1024];
+    byte[] receivedata = new byte[1024];
     
 
 
