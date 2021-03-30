2b56a2f3abc40f8e02bad5abe14f965326982e64
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-30 16:45:03 +0200

    add sendpacket DatagramPacket

--- Client.java
+++ Client.java
@@ -6,13 +6,18 @@ public class Client {
   public static void main(String[] args) throws Exception {
     InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
     int port = 5298; //set port for packet
-    byte[] senddata = new byte[1024];
-    byte[] receivedata = new byte[1024];
+    byte[] sendmessage = new byte[1024];
+    byte[] receivemessage = new byte[1024];
     DatagramSocket clientsocket = new DatagramSocket(); //Binds the DatagramSocket to any available local port.
     System.out.print("Enter text: "); //match this with GUI
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
     String input = in.readLine();//Get user input
-    System.out.println("Message to be sent: "+input); //match with GUI
+    System.out.println("Output message: "+input); //match with GUI
+    sendmessage = input.getBytes(); //Convert message string to bytes
+    DatagramPacket sendpacket = new DatagramPacket(sendmessage, sendmessage.length, ip, 1234);
+        clientsocket.send(sendpacket);
+
+
 
 
 
