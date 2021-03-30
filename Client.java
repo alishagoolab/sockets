6fdede3ad7c4c60cd64a73bcd58fff28189a083a
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-30 16:29:33 +0200

    add ip address and port

--- Client.java
+++ Client.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Client {
   public static void main(String[] args) throws Exception {
+    InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
+    int port = 5298; 
     
 
 
