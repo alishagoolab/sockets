294ce314932346d418310d52bf0c3e5dbb2daf8c
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-30 16:34:40 +0200

    Use BufferedReader to get user message

--- Client.java
+++ Client.java
@@ -5,10 +5,13 @@ import java.util.*;
 public class Client {
   public static void main(String[] args) throws Exception {
     InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
-    int port = 5298; 
+    int port = 5298; //packet port
     byte[] senddata = new byte[1024];
     byte[] receivedata = new byte[1024];
     DatagramSocket clientsocket = new DatagramSocket(); //This constructor binds the DatagramSocket to any available local port.
+    System.out.print("Enter text: "); //match this with GUI
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
 
 
   }
