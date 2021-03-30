8a07300060680302e8c395fcbac0c833d656c178
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-30 16:39:29 +0200

    set input message as string

--- Client.java
+++ Client.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class Client {
   public static void main(String[] args) throws Exception {
     InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
-    int port = 5298; //packet port
+    int port = 5298; //set port for packet
     byte[] senddata = new byte[1024];
     byte[] receivedata = new byte[1024];
-    DatagramSocket clientsocket = new DatagramSocket(); //This constructor binds the DatagramSocket to any available local port.
+    DatagramSocket clientsocket = new DatagramSocket(); //Binds the DatagramSocket to any available local port.
     System.out.print("Enter text: "); //match this with GUI
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
+    String input = in.readLine();
 
 
   }
