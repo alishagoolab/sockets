8109dbe0062cd9590981b894e1cb220f80d952bf
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-31 08:59:33 +0200

    set destination port variable

--- Client.java
+++ Client.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Client {
   public static void main(String[] args) throws Exception {
     InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
-    int port = 5298; //set port for packet
     byte[] sendmessage = new byte[1024];
     byte[] receivemessage = new byte[1024];
+    int sendport = 5298; //set destination port
 
     try {
       //Add threading here when scaling
@@ -17,14 +17,15 @@ public class Client {
       String input = in.readLine();//Get user input
       System.out.println("Output message: "+input); //match with GUI
       sendmessage = input.getBytes(); //Convert message string to bytes
-      DatagramPacket sendpacket = new DatagramPacket(sendmessage, sendmessage.length, ip, 1234);
+      //Checksum here
+      DatagramPacket sendpacket = new DatagramPacket(sendmessage, sendmessage.length, ip, sendport); 
       clientsocket.send(sendpacket);
 
       
       DatagramPacket receivepacket = new DatagramPacket(receivemessage, receivemessage.length);
       clientsocket.receive(receivepacket);
       String text = new String(receivepacket.getData());
-      System.out.println("Client input: "+ text);
+      System.out.println("Received Message: "+ text);
       clientsocket.close();
     }
     catch (SocketTimeoutException ex) {
