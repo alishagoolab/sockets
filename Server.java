d342f646c5f3cc1b7da8ceb8d8f597f9a31eed4b
rngtam002 <rngtam002@cs.uct.ac.za>
2021-03-31 09:15:55 +0200

    import io to server.java

--- Server.java
+++ Server.java
@@ -1,3 +1,11 @@
 import java.net.*;
 import java.util.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws Exception{
+      int litenport = 5298; //Match with client destination port. 
+      DatagramSocket serversocket = new DatagramSocket(listenport);
+      byte[] senddata = new byte[1024];
+      byte[] receivedata = new byte[1024];
 
