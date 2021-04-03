73d2fe098f819175ce65fd04df5b7232893f3109
rngtam002 <tamriasdev@gmail.com>
2021-04-03 10:39:37 +0200

    make display messages clearer

--- Client.java
+++ Client.java
@@ -14,6 +14,7 @@ import java.util.zip.Adler32;
 
 public class Client {
   public static void main(String[] args) throws Exception {
+    long sentChecksum;
     InetAddress ip = InetAddress.getLocalHost(); // Get IP address, can get from args instead. 
     
     int sendport = 5298; //set destination port
@@ -22,17 +23,17 @@ public class Client {
       DatagramSocket clientsocket = new DatagramSocket(); //Binds DatagramSocket to any available local port.
       while(true){
         byte[] sendmessage = new byte[1024];
-        System.out.print("Enter text: "); //match this with GUI
+        System.out.print("Enter message: "); //match this with GUI
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
         String input = in.readLine();//Get user input
-        System.out.println("Output message: "+input); //match with GUI
+        System.out.println("Sent: "+ input); //match with GUI
         sendmessage = input.getBytes(); //Convert message string to bytes
         ByteArrayInputStream bais = new ByteArrayInputStream(sendmessage);
         CheckedInputStream cis = new CheckedInputStream(bais, new Adler32());
-        byte readBuffer[] = new byte[5];
+        byte readBuffer[] = new byte[10];
         while (cis.read(readBuffer) >= 0){
-          long value = cis.getChecksum().getValue();
-          System.out.println("The value of checksum is " + value);
+          sentChecksum = cis.getChecksum().getValue();
+          System.out.println("The value of checksum is " + sentChecksum);
         }
         DatagramPacket sendpacket = new DatagramPacket(sendmessage, sendmessage.length, ip, sendport); 
         clientsocket.send(sendpacket);
