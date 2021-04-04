c1ae9e37e4e965b2d6b44282d25cc2c7b7a0c399
rngtam002 <tamriasdev@gmail.com>
2021-04-04 11:53:35 +0200

    change header

--- Client.java
+++ Client.java
@@ -2,7 +2,7 @@
  * The class Client sets up a network client for sending chat messages using the UDP protocol. 
  * A datagram socket is bound to an available port to receive messages (via datagram packets) through. 
  * Another socket is bound to the Server class's listening port to send messages (via datagram packets) to. 
- * @author RNGTAM002, RJKRAH001, GLBALI002
+ * @author RNGTAM002
  */
 
 import java.net.*;
